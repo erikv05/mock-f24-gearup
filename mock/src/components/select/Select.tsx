715293c02d1c407ee4441c831cd0cadb6e9f540c
Megan <113795130+YUUU23@users.noreply.github.com>
2024-10-07 12:17:13 -0400

    Update Select.tsx

--- mock/src/components/select/Select.tsx
+++ mock/src/components/select/Select.tsx
@@ -34,7 +34,7 @@ export interface Tab {
  */
 export function Select() {
   // TODO 2: set-up a React useState variable here to update the main display area once you click the submit button,
-  //    then, pass this variable in as a prop to SelectHistory at line 32 and uncomment it
+  //    then, pass this variable in as a prop to SelectHistory below and uncomment it
 
   return (
     <div className="min-h-[95vh] relative">
